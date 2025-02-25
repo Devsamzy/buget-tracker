e958d65d2ea278a05a1f92dabc61859b1e9603e3
Aderogba Samuel <121461245+Devsamzy@users.noreply.github.com>
2025-02-25 23:47:13 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-=======
   base:"./"
->>>>>>> d9f89f335d8b6caefae9b3b7dd215c17d9c77755
-})
+)
